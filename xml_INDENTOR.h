6995c780c5d3b5197ea694508fffd9abf7820224
AliSultan024 <128440320+AliSultan024@users.noreply.github.com>
2023-12-13 02:08:03 +0200

    Add files via upload

---
+++ xml_INDENTOR.h
@@ -0,0 +1,16 @@
+#ifndef xml_INDENTOR_H_
+#define xml_INDENTOR_H_
+#include<iostream>
+#include<fstream>
+#include<string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <cstring>
+#include "utilities.h"
+using namespace std;
+
+void xmlindentor(fstream& myfile);
+
+
+#endif
---
+++ xml_minifier.h
@@ -0,0 +1,15 @@
+#ifndef xml_minifier_H_
+#define xml_minifier_H_
+#include<iostream>
+#include<fstream>
+#include<string>
+#include <vector>
+#include <algorithm>
+#include "utilities.h"
+
+using namespace std;
+
+void xmlminifier(fstream& myfile);
+
+
+#endif
