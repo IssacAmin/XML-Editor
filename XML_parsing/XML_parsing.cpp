61fbcf5e5d9c4c680af741cc137e588497975546
poula10 <132152697+poula10@users.noreply.github.com>
2023-12-14 03:45:52 +0200

    the parsing of the xml_file

---
+++ XML_parsing/XML_parsing.cpp
@@ -0,0 +1,69 @@
+
+#include "XML_parsing.h"
+
+void XML_parsing(xmlTree& tree, fstream& file){
+	fstream myFile;
+	string line;
+	vector<string> elements = oneElementPerLine(file);
+	for(string element: elements){
+		textType type = detectType(element);  //Check the type of element in line
+		tagToNode(element,type,tree);		// Convert the tag in line to a node in the tree
+	}
+
+}
+
+textType detectType(string element){
+	if(element[0] == '<' && element[1] == '/')
+		return CLOSING_TAG;
+	else if(element[0] == '<' && element[1] != '/')
+		return OPENING_TAG;
+	else{
+		return DATA;
+	}
+}
+void tagToNode(string& element, textType type, xmlTree& tree){
+	if(type == OPENING_TAG)
+	{
+		string tagName = element.substr(1,element.find(">") - 1);
+		if(tree.empty()){							//If tree is empty
+			Node* root = new Node(0,tree.currentLevel,tagName);
+			tree.setRoot(root);
+			tree.currentNode = tree.getRoot();
+		}
+		else{
+			Node* tmp = new Node(0,tree.currentLevel, tagName);
+			tree.currentNode->add(tmp);
+			tree.currentNode = tmp;
+		}
+		tree.currentLevel++;
+
+	}
+	else if(type == DATA){
+		bool digit = true;
+		for(unsigned int i = 0; i < element.length(); i++){
+			if(!isdigit(element[i])){
+				digit = false;
+				break;
+			}
+
+		}
+		if(digit){
+			tree.currentNode->setDataInt(stoi(element));
+		}
+		else{
+			tree.currentNode->setDataString(element);
+		}
+	}
+	else{  //If it is a closing tag
+		if(tree.currentNode->getParent() != NULL)
+		{
+			tree.currentNode = tree.currentNode->getParent();
+			tree.currentLevel --;
+		}
+
+	}
+
+}
+
+
+
---
+++ XML_parsing/XML_parsing.h
@@ -0,0 +1,20 @@
+
+#ifndef XML_PARSING_H_
+#define XML_PARSING_H_
+#include <vector>
+#include <stack>
+#include <fstream>
+
+#include "../TreeStructure/xmlTree.h"
+#include "../utilities/utilities.h"
+
+typedef enum {
+	OPENING_TAG, CLOSING_TAG, DATA
+} textType;
+
+
+void XML_parsing(xmlTree& tree, fstream& file); //Function to parse the XML file into a tree
+
+textType detectType(string element); //Detect the type of element (Opening tag, Closing tag, Data)
+void tagToNode(string& element, textType type, xmlTree& tree); //Convert each tag into a node in the tree
+#endif /* XML_PARSING_H_ */
